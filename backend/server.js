6e0b8fd790ab69d61df6f7b8a42fb350a9462a6a
Ishwari-Jadhav <ishwarijadhav.ij05@gmail.com>
2026-04-09 16:42:04 +0530

    Added changes

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,7 @@ const startServer = async () => {
   try {
     console.log("Connecting to MongoDB...");
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected ✅");
 
